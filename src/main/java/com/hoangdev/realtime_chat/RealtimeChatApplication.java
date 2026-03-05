14a81818d30ed39304a818bbe9a0583f066f621d
Nguyễn  Việt Hoàng <beto@MacBook-Air-cua-Pham.local>
2026-03-06 02:55:20 +0900

    Initial commit

---
+++ src/main/java/com/hoangdev/realtime_chat/RealtimeChatApplication.java
@@ -0,0 +1,16 @@
+package com.hoangdev.realtime_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeChatApplication {
+	public static void main(String[] args) {
+        SpringApplication.run(RealtimeChatApplication.class, args);
+
+
+
+
+	}
+
+}
